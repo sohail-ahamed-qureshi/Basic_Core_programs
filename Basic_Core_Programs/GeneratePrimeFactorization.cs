f4efcef5e7adbe1f4c35f625afb24da7b5b21949
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-26 17:38:27 +0530

    [sohail] Add: Prime Factorization program

---
+++ Basic_Core_Programs/GeneratePrimeFactorization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Core_Programs
+{
+    class GeneratePrimeFactorization
+    {
+        public void PrimeFactor(int number)
+        {
+            //number should be greater than 1.
+            while(number % 2 ==0)
+            {
+                Console.Write(2 +" ");
+                number /= 2;
+            }
+            
+            for (int i=3; i<= number; i++)
+            {
+                while(number % i == 0)
+                {
+                    Console.Write(i +" ");
+                    
+                    number /= i;
+                }
+            }
+
+            if(number > 2)
+            {
+                Console.Write(number);
+            }
+
+        }
+    }
+}
--- Basic_Core_Programs/Program.cs
+++ Basic_Core_Programs/Program.cs
@@ -12,7 +12,7 @@ namespace Basic_Core_Programs
             Console.WriteLine("Press 2. for Leap year. ");
             Console.WriteLine("Press 3. for Power of 2. ");
             Console.WriteLine("Press 4. for Harmonic Number. ");
-            Console.WriteLine("Press 5. for Flip coin. ");
+            Console.WriteLine("Press 5. for Prime factorization. ");
             Console.WriteLine("Press 6. for Flip coin. ");
             Console.WriteLine("Press 7. for Flip coin. ");
             Console.WriteLine("Press 8. for Flip coin. ");
@@ -42,6 +42,11 @@ namespace Basic_Core_Programs
                     //Harmonic Number
                     GenerateHarmonicNumber har = new GenerateHarmonicNumber();
                     har.HarmonicNumber(8);
+                    break;
+                case 5:
+                    //Prime Factorization
+                    GeneratePrimeFactorization prime = new GeneratePrimeFactorization();
+                    prime.PrimeFactor(315);
                     break;
                 default:
                     Console.WriteLine("invalid selection made.");
