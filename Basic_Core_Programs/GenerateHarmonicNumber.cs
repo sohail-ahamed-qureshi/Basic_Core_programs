df111b34c73bf54ccb8c1437226d185ccd7e4126
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-26 17:10:08 +0530

    [sohail] Add: Harmonic Number & Refactored Main program

---
+++ Basic_Core_Programs/GenerateHarmonicNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Basic_Core_Programs
+{
+    class GenerateHarmonicNumber
+    {
+        public int HarmonicNumber(int N)
+        {
+            //variables
+            float harmonic = 1;
+            //check N != 0
+            if (N <= 0)
+            {
+                Console.WriteLine($"Harmonic Number for {N} is: 0");
+                return 0;
+            }
+            // generate Harmonic number for N
+            
+            for(int i =2; i<= N; i++)
+            {
+                harmonic +=  (float) 1 / i;
+                
+            }
+            Console.WriteLine($"= {harmonic}");
+            return 0;
+        }
+    }
+}
--- Basic_Core_Programs/Program.cs
+++ Basic_Core_Programs/Program.cs
@@ -7,17 +7,55 @@ namespace Basic_Core_Programs
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Programs");
-            // flip coin 
-            FlipCoin flipCoin = new FlipCoin();
-            flipCoin.PercentageFlips(10);
+            Console.WriteLine("Enter your choice: ");
+            Console.WriteLine("Press 1. for Flip coin. ");
+            Console.WriteLine("Press 2. for Leap year. ");
+            Console.WriteLine("Press 3. for Power of 2. ");
+            Console.WriteLine("Press 4. for Harmonic Number. ");
+            Console.WriteLine("Press 5. for Flip coin. ");
+            Console.WriteLine("Press 6. for Flip coin. ");
+            Console.WriteLine("Press 7. for Flip coin. ");
+            Console.WriteLine("Press 8. for Flip coin. ");
+            Console.WriteLine("Press 9. for Flip coin. ");
+            Console.WriteLine("Press 10. for Flip coin. ");
 
-            //leap year
-            CheckLeapYear leap = new CheckLeapYear();
-            leap.LeapYear(2002);
+            int selection = Convert.ToInt32( Console.ReadLine());
 
-            //power of 2
-            PowerOf2 pow = new PowerOf2();
-            pow.CheckPower(32);
+            switch (selection)
+            {
+                case 1:
+                    // flip coin 
+                    FlipCoin flipCoin = new FlipCoin();
+                    flipCoin.PercentageFlips(10);
+                    break;
+                case 2:
+                    //leap year
+                    CheckLeapYear leap = new CheckLeapYear();
+                    leap.LeapYear(2002);
+                    break;
+                case 3:
+                    //power of 2
+                    PowerOf2 pow = new PowerOf2();
+                    pow.CheckPower(32);
+                    break;
+                case 4:
+                    //Harmonic Number
+                    GenerateHarmonicNumber har = new GenerateHarmonicNumber();
+                    har.HarmonicNumber(8);
+                    break;
+                default:
+                    Console.WriteLine("invalid selection made.");
+                    break;
+
+
+
+            }
+
+
+
+
+
+
         }
     }
 }
