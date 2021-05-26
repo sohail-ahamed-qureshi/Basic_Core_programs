95e3ffb879b68dd0c64897a7a556e66157394bad
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-26 13:15:10 +0530

    [sohail] Add: Welcome message for Basic Core Problems

---
+++ Basic_Core_Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Basic_Core_Programs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Programs");
+        }
+    }
+}
