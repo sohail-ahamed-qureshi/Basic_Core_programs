cad76cdd621f1b8612ad4dc284d48deac664f295
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-27 08:38:53 +0530

    [sohail] Refactor: Update in Largest Among 3 numbers

--- Basic_Core_Programs/CheckLargest.cs
+++ Basic_Core_Programs/CheckLargest.cs
@@ -8,16 +8,19 @@ namespace Basic_Core_Programs
     {
         public void Largest(int number1, int number2, int number3)
         {
+            if(number1 == number2 == number3)
+            {
+                Console.WriteLine("All inputs are equal");
+            }
             if(number1 > number2 && number1 > number3)
             {
                 Console.WriteLine($"{number1} is greater");
             }
-            else if(number2 > number1 && number2 > number3)
+             if(number2 > number1 && number2 > number3)
             {
                 Console.WriteLine($"{number2} is greater");
-
             }
-            else
+             if(number3 > number1 && number3 > number2)
             {
                 Console.WriteLine($"{number3} is greater");
             }
